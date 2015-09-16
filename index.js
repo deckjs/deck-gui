23a3f7673dd588f2686a3d48283213c831b59e2d
Mihai Dima <mihaizn@gmail.com>
2015-09-16 17:37:22 +0300

    nw as external package

--- index.js
+++ index.js
@@ -3,15 +3,18 @@
 var path = require('path');
 var spawn = require('win-spawn');
 var nw = require('nw').findpath();
+// var nw = require('@deck/gui-app').findpath();
 var app =  path.join(__dirname, 'nw-app');
 
 module.exports = function (url) {
-
+process.stdout.write('url ', url);
+process.stdout.write('app ', app);
+process.stdout.write('cwd ', path.dirname(require.resolve('@deck/gui-app'));
   var stream = spawn(nw, [
     '.',
     url
   ], {
-    cwd: path.resolve(__dirname, 'nwapp'),
+    cwd: path.dirname(require.resolve('@deck/gui-app')),
     env: process.env,
     //stdio: 'inherit'
   });
