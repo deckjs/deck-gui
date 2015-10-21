932142dced975e36df7ce765a6b09410c0373932
davidmarkclements <huperekchuno@googlemail.com>
2015-10-21 19:41:21 +0100

    electron

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 'use strict'
 
-var path = require('path')
-var spawn = require('win-spawn')
-var nw = require('nw').findpath()
+var app = require('app')
+var BrowserWindow = require('browser-window')
+var scr = () => require('screen')
+var configs = [
+  {
+    toolbar: false,
+    center: true,
+    width: 480,
+    height: 342
+  },
+  {
+    toolbar: false,
+    fullscreen: true
+  }
+]
 
-module.exports = function (url) {
-  var stream = spawn(nw, [
-    '.',
-    url
-  ], {
-    cwd: path.dirname(require.resolve('@deck/gui-app')),
-    env: process.env
+app.on('ready', () => {
+  app.ready = true
+  app.windows = scr().getAllDisplays().slice(0, 2)
+    .map((display, ix) => {
+      var win = new BrowserWindow(Object.assign({}, display.bounds, configs[ix]))
+      return win
+    })
+  app.windows.forEach(win => {
+    win.loadUrl('file://' + __dirname + '/loading.html')
+    win.on('close', process.exit)
   })
+})
 
-  return stream
+module.exports = function open (url) {
+  if (!app.ready) return app.on('ready', () => open(url))
+  app.windows.forEach(win => win.loadUrl(url))
 }
